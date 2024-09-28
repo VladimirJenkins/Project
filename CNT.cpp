5db3a60b1e4f66f2b31becff2339be5e13f5e834
DuyHung <damduyhung242007@gmail.com>
2024-09-28 19:43:52 +0700

    Add files via upload

---
+++ CNT.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+	int L, R;
+	vector <int> socantim;
+	cout << "Nhập giá trị đầu: ";
+	do {
+		cin >> L;
+	} while ( L < 1 );
+	cout << "Nhập giá trị cuối: ";
+	do {
+		cin >> R;
+	} while (L >= R);
+	for (int j = L;j < R;j++) {
+		int u = L % 10;
+		int y = L / 10;
+		int z = u + y ;
+		if ( z == 2 || z == 3 || z == 5 || z == 7) {
+			socantim.push_back(L);
+		}
+		L++;
+	}
+	for (int k : socantim) {
+		cout << k << endl;
+	}
+	return 0;
+}
